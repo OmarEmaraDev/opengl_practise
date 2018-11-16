46e11bcf6328859ec03a493704a14b653137a130
OmarSquircleArt <omar.squircleart@gmail.com>
2018-11-16 15:47:59 +0200

    Refactor Code, More Modular.

--- electric_field/electric_field.c
+++ electric_field/electric_field.c
@@ -18,42 +18,116 @@ GLfloat chargePositions[MAXCOUNT * 2];
 GLFWcursor* crossCursor;
 GLFWcursor* regularCursor;
 
-char isNear(double x1, double y1, double x2, double y2) {
-    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) < THRESHOLD * THRESHOLD;
+typedef struct {
+    double x;
+    double y;
+} mousePos;
+mousePos mPos;
+
+static void mousePosToDeviceCooridinates() {
+    mPos.x = (mPos.x / WIDTH - 0.5) * 2;
+    mPos.y = - (mPos.y / HEIGHT - 0.5) * 2;
+}
+
+char isNearAny(double x, double y) {
+    if (chargeCount == 0) return 0;
+    char isNearAny = 0;
+    for (size_t i = 0; i < chargeCount; i++) {
+        double x2 = chargePositions[i * 2];
+        double y2 = chargePositions[i * 2 + 1];
+        if ((x - x2) * (x - x2) + (y - y2) * (y - y2) < THRESHOLD * THRESHOLD){
+            isNearAny = 1;
+        }
+    }
+    return isNearAny;
 }
 
 static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
     if (button != GLFW_MOUSE_BUTTON_LEFT) return;
     if (action == GLFW_PRESS) {
-        double xPos, yPos;
-        glfwGetCursorPos(window, &xPos, &yPos);
-        xPos = (xPos / WIDTH - 0.5) * 2;
-        yPos = - (yPos / HEIGHT - 0.5) * 2;
-        char isNearAny = 0;
-        for (size_t i = 0; i < chargeCount; i++) {
-            if (isNear(xPos, yPos, chargePositions[i * 2], chargePositions[i * 2 + 1])){
-                isNearAny = 1;
-            }
-        }
-        if (!isNearAny) {
-            chargePositions[chargeCount * 2] = xPos;
-            chargePositions[chargeCount * 2 + 1] = yPos;
+        glfwGetCursorPos(window, &mPos.x, &mPos.y);
+        mousePosToDeviceCooridinates();
+        if (!isNearAny(mPos.x, mPos.y)) {
+            chargePositions[chargeCount * 2] = mPos.x;
+            chargePositions[chargeCount * 2 + 1] = mPos.y;
             chargeCount++;
         }
     }
 }
 
-static void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
+static void cursorPositionCallback(GLFWwindow* window, double xPos, double yPos)
 {
-    double xPos = (xpos / WIDTH - 0.5) * 2;
-    double yPos = - (ypos / HEIGHT - 0.5) * 2;
-    char isNearAny = 0;
-    for (size_t i = 0; i < chargeCount; i++) {
-        if (isNear(xPos, yPos, chargePositions[i * 2], chargePositions[i * 2 + 1])){
-            isNearAny = 1;
-        }
-    }
-    glfwSetCursor(window, isNearAny ? crossCursor : regularCursor);
+    mPos.x = xPos; mPos.y = yPos;
+    mousePosToDeviceCooridinates();
+    glfwSetCursor(window, isNearAny(mPos.x, mPos.y) ? crossCursor : regularCursor);
+}
+
+typedef struct {
+    GLuint chargesLocation;
+    GLuint chargeCountLocation;
+    GLuint shaderProgram;
+    GLuint vbo;
+    GLuint ibo;
+} potentialField;
+potentialField field;
+
+void preparePotentialField() {
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    GLfloat vertices[] = {
+        -1.,  1.,
+         1.,  1.,
+         1., -1.,
+        -1., -1.
+    };
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    field.vbo = vbo;
+
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+    GLuint indices[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    field.ibo = ibo;
+
+    const GLchar* vertexSource = readShaderSource("vertex_shader.vert");
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+    free((void*)vertexSource);
+
+    const GLchar* fragmentSource = readShaderSource("potential_field_fragmenet_shader.frag");
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+    free((void*)fragmentSource);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    field.shaderProgram = shaderProgram;
+
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    field.chargesLocation = glGetUniformLocation(shaderProgram, "charges");
+    field.chargeCountLocation = glGetUniformLocation(shaderProgram, "chargeCount");
+}
+
+void drawPotentialField() {
+    glUseProgram(field.shaderProgram);
+    glUniform2fv(field.chargesLocation, chargeCount, chargePositions);
+    glUniform1i(field.chargeCountLocation, chargeCount);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
 
 int main(void)
@@ -69,67 +143,21 @@ int main(void)
     regularCursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
     glfwSetCursorPosCallback(window, cursorPositionCallback);
 
-    GLuint potentialFieldvbo;
-    glGenBuffers(1, &potentialFieldvbo);
-    GLfloat vertices[] = {
-        -1.,  1.,
-         1.,  1.,
-         1., -1.,
-        -1., -1.
-    };
-    glBindBuffer(GL_ARRAY_BUFFER, potentialFieldvbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    GLuint potentialFieldibo;
-    glGenBuffers(1, &potentialFieldibo);
-    GLuint indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, potentialFieldibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    const GLchar* vertexSource = readShaderSource("vertex_shader.vert");
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
-    free((void*)vertexSource);
-
-    const GLchar* fragmentSource = readShaderSource("potential_field_fragmenet_shader.frag");
-    GLuint potentialFieldFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(potentialFieldFragmentShader, 1, &fragmentSource, NULL);
-    glCompileShader(potentialFieldFragmentShader);
-    free((void*)fragmentSource);
-
-    GLuint potentialFieldShaderProgram = glCreateProgram();
-    glAttachShader(potentialFieldShaderProgram, vertexShader);
-    glAttachShader(potentialFieldShaderProgram, potentialFieldFragmentShader);
-    glLinkProgram(potentialFieldShaderProgram);
-    glUseProgram(potentialFieldShaderProgram);
-
-    GLint posAttrib = glGetAttribLocation(potentialFieldShaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-    GLint chargesLocation = glGetUniformLocation(potentialFieldShaderProgram, "charges");
-    GLint chargeCountLocation = glGetUniformLocation(potentialFieldShaderProgram, "chargeCount");
+    preparePotentialField();
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
-        glUniform2fv(chargesLocation, chargeCount, chargePositions);
-        glUniform1i(chargeCountLocation, chargeCount);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        drawPotentialField();
 
         glfwSwapBuffers(window);
         glfwWaitEvents();
     }
 
-    glDeleteProgram(potentialFieldShaderProgram);
-    glDeleteShader(potentialFieldFragmentShader);
-    glDeleteShader(vertexShader);
-    glDeleteBuffers(1, &potentialFieldvbo);
+    glDeleteProgram(field.shaderProgram);
+    glDeleteBuffers(1, &field.vbo);
+    glDeleteBuffers(1, &field.ibo);
 
     glfwTerminate();
     return 0;
